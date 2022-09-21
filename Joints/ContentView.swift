becb776cebec0aa12ac563c593c26735399b970d
Cole Thorpen <thorpencole@gmail.com>
2022-09-21 16:30:18 -0700

    first commit

---
+++ Joints/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Joints
+//
+//  Created by Cole Thorpen on 9/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Joints/JointsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JointsApp.swift
+//  Joints
+//
+//  Created by Cole Thorpen on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct JointsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
