21391f4cb25bd36cdb9eb87c01a823a06462a63d
Cole Thorpen <thorpencole@gmail.com>
2022-09-21 16:59:11 -0700

    initial

---
+++ Joints/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Joints
+//
+//  Created by Cole Thorpen on 9/21/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("maps baby")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
